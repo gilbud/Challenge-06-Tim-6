2ab031b8abb84af3ea41a972cf556806beb94530
budi <gilangbb1010@gmail.com>
2023-05-20 14:06:22 +0700

    redux detail

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "@reduxjs/toolkit";
 import movieReducers from "./movieReducers";
 import authReducer from "./authReducer";
 import searchReducer from "./searchReducer";
+import detailReducer from "../../../../ch6 fix/src/redux/reducers/detailReducer";
 
 export default combineReducers({
   movies: movieReducers,
   auth: authReducer,
   search: searchReducer,
+  detail: detailReducer,
 });
